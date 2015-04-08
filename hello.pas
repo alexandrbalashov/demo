1598487f07ccc9b30448c19fb5c876906933063f
alexandrbalashov <abalashow2002@mail.ru>
2015-04-08 16:22:27 +0400

    добавил процедуру создающую текстовый фаил из 100000 однозначных целых чисел

--- hello.pas
+++ hello.pas
@@ -1,3 +1,18 @@
+const PATH = 'data.txt';
+
+procedure writefile();
+var f:text;
+    v,i:integer;
 begin
-  writeln('hello world');
+  assign(f,PATH);
+  append(f);
+  for i:=1 to 100000 do begin
+    v:=random(10);
+    writeln(f,v);
+  end;
+  close(f);
+end;
+
+begin
+  writefile();
 end.  
