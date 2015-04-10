2ef4f0ffec9b1f51779d0c4ea586885f2a12acc9
alexandrbalashov <abalashow2002@mail.ru>
2015-04-10 14:48:51 +0400

    анписали процедуру чтения файла и вывода его на экран

--- hello.pas
+++ hello.pas
@@ -1,5 +1,5 @@
 const PATH = 'data.txt';
-      N = 1000000;
+      N = 10000;
 
 procedure writefile();
 var f:text;
@@ -13,11 +13,29 @@ begin
   end;
   close(f);
 end;
-  
+
+procedure readfile();
+var f:text;
+    a:integer;
+begin
+  assign(f,PATH);
+  reset(f);
+  while (not eof(f))do begin
+    readln(f,a);
+    writeln(a);
+  end;
+  close(f);
+end;  
+    
 var time:longint;
 begin
   time:= milliseconds();
   writefile();
+  time:= milliseconds()-time;
+  writeln(time);
+  
+  time:= milliseconds();
+  readfile();
   time:= milliseconds()-time;
   writeln(time);
 end.  
