145783bde12a29138a91a01ff665df84b5cf7874
alexandrbalashov <abalashow2002@mail.ru>
2015-04-10 14:26:22 +0400

    Количество записей в файле перенесли в константу

--- hello.pas
+++ hello.pas
@@ -1,18 +1,23 @@
 const PATH = 'data.txt';
+      N = 1000000;
 
 procedure writefile();
 var f:text;
     v,i:integer;
 begin
   assign(f,PATH);
-  append(f);
-  for i:=1 to 100000 do begin
+  rewrite(f);
+  for i:=1 to N do begin
     v:=random(10);
     writeln(f,v);
   end;
   close(f);
 end;
-
+  
+var time:longint;
 begin
+  time:= milliseconds();
   writefile();
+  time:= milliseconds()-time;
+  writeln(time);
 end.  
